a8d63f46c07ed5dabf97332b33af28794ce3c3ef
Atepa <veratis_yasin@hotmail.com>
2023-10-15 13:39:52 +0300

    fix parameters

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const routes=require("./routes/routes");
 const db=require("./data/db");
 const rateLimit=require("./middleware/rate-limit");
 
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(rateLimit);
 
 app.use("/",routes);
+app.use("*",(req,res)=>{
+    res.status(404).send("Wrong Url");
+})
 
 app.listen(port, ()=>{
     console.log("App listening on port "+port);
